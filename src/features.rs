9858d937f063734ee038e39cbb50f9240d30d7dd
darcy <acednes@gmail.com>
2023-10-17 09:48:53 +1100

    fix meta ?

--- src/features.rs
+++ src/features.rs
@@ -31,42 +31,45 @@ pub struct Meta<'a> {
 /// until added to ibex crate
 pub fn use_meta(meta: Meta) -> View {
     view! {
-        HEAD {
-            meta [charset="utf-8"]/
-            meta [name="viewport", content="width=device-width, initial-scale=1"]/
+            HEAD {
+                meta [charset="utf-8"]/
+                meta [name="viewport", content="width=device-width, initial-scale=1"]/
 
-            [if let Some(url) = meta.url { view!{
-                meta [name="url",        content=url]/
-                meta [property="og:url", content=url]/
-            }} else { view! {}}]
+                [if let Some(url) = meta.url { view!{
+                    meta [name="url",        content=url]/
+                    meta [property="og:url", content=url]/
+                }} else { view! {}}]
 
-            [if let Some(title) = meta.title { view!{
-                meta [itemprop="name",     content=title]/
-                meta [property="og:title", content=title]/
-                meta [name="title",        content=title]/
-            }} else { view! {}}]
+                [if let Some(title) = meta.title { view!{
+                    meta [itemprop="name",     content=title]/
+                    meta [property="og:title", content=title]/
+                    meta [name="title",        content=title]/
+                }} else { view! {}}]
 
-            [if let Some(desc) = meta.desc{ view!{
-                meta [name="description",         content=desc]/
-                meta [itemprop="description",     content=desc]/
-                meta [property="og:description",  content=desc]/
-                meta [name="twitter:description", content=desc]/
-            }} else { view! {}}]
+                [if let Some(desc) = meta.desc{ view!{
+                    meta [name="description",         content=desc]/
+                    meta [itemprop="description",     content=desc]/
+                    meta [property="og:description",  content=desc]/
+                    meta [name="twitter:description", content=desc]/
+                }} else { view! {}}]
 
-            [if let Some(image) = meta.image { view!{
-                meta [name="image",         content=image]/
-                meta [itemprop="image",     content=image]/
-                meta [property="og:image",  content=image]/
-                meta [name="twitter:image", content=image]/
-            }} else { view! {}}]
+                [if let Some(image) = meta.image { view!{
+                    meta [name="image",         content=image]/
+                    meta [itemprop="image",     content=image]/
+                    meta [property="og:image",  content=image]/
+                    meta [name="twitter:image", content=image]/
+                }} else { view! {}}]
 
-            [if let Some(author) = meta.author { view!{
-                meta [name="author", content=author]/
-            }} else { view! {}}]
+                [if let Some(author) = meta.author { view!{
+                    meta [name="author", content=author]/
+                }} else { view! {}}]
 
-            [if let Some(color) = meta.color { view!{
-                meta [name="theme-color", content=color]/
-            }} else { view! {}}]
+                [if let Some(color) = meta.color { view!{
+                    meta [name="theme-color", content=color]/
+                }} else { view! {}}]
+
+                meta [property="og:type", content="website"]/
+                meta [name="twitter:card", content="summary_large_image"]/
+            }
         }
-    }
 }
